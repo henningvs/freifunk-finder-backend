287e0be894d2d515aeab66e4761873456886029d
Henning von See <henning@vonsee.de>
2017-03-19 17:25:12 +0100

    Removed everything which is not for syncing the nodes -> api will go to different project

--- sync.js
+++ sync.js
@@ -1,120 +1,181 @@
-var request = require('request'),
-    jsonschema = require('jsonschema'),
-    MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
+const request = require('request');
+const jsonschema = require('jsonschema');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const winston = require('winston');
 
-var mongodb = 'mongodb://localhost:27017/fffinder';
-var central = 'https://raw.githubusercontent.com/freifunk/directory.api.freifunk.net/master/directory.json';
+const mongodb = 'mongodb://localhost:27017/fffinder';
+const central = 'https://raw.githubusercontent.com/freifunk/directory.api.freifunk.net/master/directory.json';
 
-var ffcities = {};
-if(process.env.NODE_ENV === 'development')console.log("Get JSON for: "+central);
 
-request(central, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        ffcities = JSON.parse(body);
-    }else{
-        ffcities = {};
-    }
-
-    Object.keys(ffcities).forEach(function(key) {
-    var url = ffcities[key];
-
-        request(url, function (error, response, body) {
-
-            ffcity = {};
+const getCentralList = function getCentralList() {
+    if(process.env.NODE_ENV !== 'production')winston.info("Get JSON for: "+central);
+    const centralListPromise = new Promise((resolve, reject) => {
+        request(central, function (error, response, body) {
+            let ffcities = {};
             if (!error && response.statusCode == 200) {
-                ffcity = JSON.parse(body);
-            }
-
-            ffcity["dir_name"] = this.key;
-            parseFFCity(ffcity);
-        }.bind({key:key}));
-    });
-
-});
-
-var parseFFCity = function(ffcity){
-
-    var nodeMaps = ffcity.nodeMaps;
-    if(nodeMaps!=undefined){
-        parseNodeMap(nodeMaps, ffcity);
-    }
-
-};
-
-var parseNodeMap = function (nodeMaps, ffcity) {
-
-    nodeMaps.forEach(function(nodeMap) {
-        request(nodeMap.url, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var validate = jsonschema.validate;
-                try{
-                    nodes = JSON.parse(body);
-                }catch (error){
-                    nodes = {};
-                }
-
+                ffcities = JSON.parse(body);
             }else{
-                nodes = {};
+                winston.error(`An error occured when calling ${central}`, error);
+                return reject(error);
             }
-            if(nodes.nodes != undefined){
-                console.log("Loaded Nodes for: "+ffcity.dir_name);
-                writeToDB(ffcity, nodes);
-            }
+            return resolve(ffcities);
         });
     });
+    return centralListPromise;
 };
 
-var writeToDB = function (ffcity, nodes) {
+const getSingleFFCity = function getSingleFFCity(ffcities) {
+    winston.info(`Retrieve all cities: ${Object.keys(ffcities).length}`);
+    const cityPromises = [];
+    let i = 0;
+    Object.keys(ffcities).forEach(function(key) {
+        if(i < 3) {
+            i += 1;
+            const url = ffcities[key];
 
+            const cityPromise = new Promise((resolve, reject) => {
+                request(url, function (error, response, body) {
+
+                    ffcity = {};
+                    if (!error && response.statusCode == 200) {
+                        ffcity = JSON.parse(body);
+                    } else {
+                        winston.error(`An error occured when calling ${url}`, error);
+                        return reject(error);
+                    }
+
+                    ffcity["dir_name"] = this.key;
+                    return resolve(ffcity);
+                    //parseFFCity(ffcity);
+                }.bind({key:key}));
+            });
+            cityPromises.push(cityPromise);
+        }
+    });
+
+    const allPromise = Promise.all(cityPromises.map(p => p.catch(e => e)));
+    return allPromise;
+};
+
+
+const getNodeMaps = function (ffcities) {
+    winston.info(`Retrieve all nodemaps for cities [${ffcities.length}]`);
+    const nodemapsPromise = new Promise((resolve, reject) => {
+
+        const nodemapsPromiseCollection = [];
+        ffcities.forEach((ffcity) => {
+            let nodeMaps = ffcity.nodeMaps;
+            if(nodeMaps === undefined){
+                winston.error(new Error(`No nodemap defined for ${ffcity.name}.`));
+                nodeMaps = [];
+            }
+
+            nodeMaps.forEach(function(nodeMap) {
+                const nodemapPromise = new Promise((resolve, reject) => {
+                    request(nodeMap.url, function (error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            // const validate = jsonschema.validate;
+                            try{
+                                nodes = JSON.parse(body);
+                            }catch (error){
+                                return reject(error);
+                            }
+
+                        } else{
+                            return reject(error);
+                        }
+
+                        if(nodes.nodes != undefined){
+                            if(!ffcity.nodes)ffcity.nodes = [];
+                            ffcity.nodes.push(nodes);
+                            winston.info("Loaded Nodes for: "+ffcity.dir_name);
+                            // writeToDB(ffcity, nodes);
+                            return resolve(ffcity);
+                        }
+                        return reject(`No nodes found: ${ffcity.dir_name}`);
+                    });
+                });
+                nodemapsPromiseCollection.push(nodemapPromise);
+            });
+        });
+        return resolve(Promise.all(nodemapsPromiseCollection.map(p => p.catch(e => e))));
+    });
+    return nodemapsPromise;
+};
+
+
+
+const writeToDB = function (ffcities) {
     MongoClient.connect(mongodb, function(err, db) {
         assert.equal(null, err);
-        var collection = db.collection('fffinder_nodes', function(err, collection) {});
+        const collection = db.collection('fffinder_nodes', function(err, collection) {});
         collection.createIndex( { "createdAt": 1 }, { expireAfterSeconds: 86400 } );
 
-        var updatedDocs = 0;
-        var citydocs = [];
-        for (var i = 0; i < nodes.nodes.length; i++){
-            var node = nodes.nodes[i];
-            var nodecityinfo = {"dir_name": ffcity.dir_name};
-            node["ffcity"] = nodecityinfo;
-            node["createdAt"] = new Date();
+        const updatedDocs = 0;
+        const citydocs = [];
+        ffcities.forEach((ffcity) => {
+            if(ffcity.nodes !== undefined && Array.isArray(ffcity.nodes)) {
+                for (let a = 0; a < ffcity.nodes.length; a++){
+                    const nodeArray = ffcity.nodes[a];
+                    for (let i = 0; i < nodeArray.nodes.length; i++){
+                        const node = nodeArray.nodes[i];
+                        const nodecityinfo = {"dir_name": ffcity.dir_name};
+                        node["ffcity"] = nodecityinfo;
+                        node["createdAt"] = new Date();
 
-            var updateCommand = {
-            updateOne: {
-                    "filter": {
-                        ffcity: {
-                            dir_name: node.ffcity.dir_name
-                        },
-                        id: node.id
-                    },
-                    "update": {$set: node},
-                    "upsert": true,
-                    "writeConcern": {w:1}
-                }
-            };
+                        const updateCommand = {
+                            updateOne: {
+                                "filter": {
+                                    ffcity: {
+                                        dir_name: node.ffcity.dir_name
+                                    },
+                                    id: node.id
+                                },
+                                "update": {$set: node},
+                                "upsert": true,
+                                "writeConcern": {w:1}
+                            }
+                        };
 
-            citydocs.push(updateCommand);
-            /*collection.updateOne({
-                ffcity: {
-                    dir_name: node.ffcity.dir_name
-                },
-                id: node.id},
-                {$set:node},
-                {upsert:true, w:1},
-                function(err, result) {
-                    updatedDocs = updatedDocs+1;
-                    if(updatedDocs==nodes.nodes.length){
-                        db.close();
-                        console.log("Wrote Nodes to MongoDB: "+ffcity.dir_name);
+                        citydocs.push(updateCommand);
+                        /*collection.updateOne({
+                         ffcity: {
+                         dir_name: node.ffcity.dir_name
+                         },
+                         id: node.id},
+                         {$set:node},
+                         {upsert:true, w:1},
+                         function(err, result) {
+                         updatedDocs = updatedDocs+1;
+                         if(updatedDocs==nodes.nodes.length){
+                         db.close();
+                         winston.info("Wrote Nodes to MongoDB: "+ffcity.dir_name);
+                         }
+                         });*/
                     }
-                });*/
-        }
+                }
+            }
 
+        });
 
-        var bw = collection.bulkWrite(citydocs);
-        console.log(bw);
-        console.log("Wrote Nodes to MongoDB: "+ffcity.dir_name);
+        const bw = collection.bulkWrite(citydocs);
+        winston.info(bw);
+        winston.info("Wrote Nodes to MongoDB: "+ffcity.dir_name);
 
     });
 };
+
+
+const centralList = getCentralList();
+centralList
+    .then(getSingleFFCity)
+    .then(getNodeMaps)
+    .then(writeToDB)
+    .then((result) => {
+        winston.info(result);
+    })
+    .catch((error) => {
+        winston.error(error);
+    });
+
